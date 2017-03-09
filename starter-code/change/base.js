7e017362b9cfa5f2c36d41c970ab9150779266b0
komal jadvani <komaljadvani3@gmail.com>
2017-03-09 08:55:41 -0600

    jQuery

--- starter-code/change/base.js
+++ starter-code/change/base.js
@@ -3,5 +3,13 @@ console.log("Sanity Check: JS is working!");
 $(document).ready(function(){
 
   $("#time").text( Date.now() );
+  $( "input" ).keyup(function() {
+  var num1 = $('#left').val();
+   var num2 = $('#right').val();
+   var newValue = Number(num1) + Number(num2);
+   $('#total').val(newValue);
 
-})
+  
+ })
+ .keyup();
+});
--- starter-code/click/base.js
+++ starter-code/click/base.js
@@ -1,5 +1,11 @@
 console.log("Sanity Check: JS is working!");
-
 $(document).ready(function(){
+  $( "span" ).click(function() {
+    var num = $(this).attr('id');
+    var text = $( "span" ).eq(Number(num - 1)).text();
 
-})
+    $('#list').text(text);
+
+  });
+  $("#time").text( Date.now() );
+});
